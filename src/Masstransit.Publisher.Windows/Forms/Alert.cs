88b6b0ba5ac5b82fc36c772b0e5027699215c402
pollaris.ncamara <pollaris.ncamara@terceiros.mag.com.br>
2024-09-14 17:29:40 -0300

    feat(kafka): active kafka broker

---
+++ src/Masstransit.Publisher.Windows/Forms/Alert.cs
@@ -0,0 +1,10 @@
+﻿namespace Masstransit.Publisher.Windows.Forms
+{
+    public static class Alert
+    {
+        public static void Show(string message,string title, MessageBoxButtons button, MessageBoxIcon icon)
+        {
+            MessageBox.Show(message, title, button, icon);
+        }
+    }
+}
